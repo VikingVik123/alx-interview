af829f71ffe1059fb8e8efdccb102b0ae8c50371
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-16 03:26:10 +0100

    added commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,13 +6,15 @@ Given a number n
 write a method that calculates the fewest number of operations
 to result in exactly n H characters in the file.
 """
+
+
 def minOperations(n):
     """
     Function initialization
     """
     if n == 0:
         return 0
-    
+
     operation = 0
     factor = 2
     while n > 1:
