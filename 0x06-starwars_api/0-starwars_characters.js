b99f6f9f389056d4122ed3d59663fa4033d6e038
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-08 12:16:43 +0100

    tried 1st

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const url = "https://swapi-api.hbtn.io/api"
+const request = require('request');
+
+if (process.argv.length > 2) {
+    request(`${url}/films/${movieId}/`, (err, _, body) => {
+        if (err) {
+          console.log(err);
+        }
+        const characters = JSON.parse(body).characters;
+        const charactersName = characters.map(
+            url => new Promise((resolve, reject) => {
+            request(url, (promiseErr, __, charactersReqBody) => {
+                if (promiseErr) {
+                    reject(promiseErr);
+                }
+                resolve(JSON.parse(charactersReqBody).name);
+            });
+        }));
+
+    Promise.all(charactersName)
+        .then(names => console.log(names.join('\n')))
+        .catch(allErr => console.log(allErr));
+    });
+}
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+# 0x06. Star Wars API
+
+Uses javascript to fetch data on Star Wars cast by querying API's
